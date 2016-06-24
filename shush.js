f15f0d4e90b70fb8d6ce566d21a36867de3c110a
Meredith Roman <meredith.c.roman@gmail.com>
2016-06-24 15:40:34 -0400

    shush module

---
+++ shush.js
@@ -0,0 +1,3 @@
+var exec = require('child_process').exec;
+
+module.exports = exec("curl -X POST --data-urlencode 'payload={\"channel\": \"#juniors\", \"username\": \"SHHHHHHHH\", \"text\": \"hello from inside node\", \"icon_emoji\": \":ghost:\"}' https://hooks.slack.com/services/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");
